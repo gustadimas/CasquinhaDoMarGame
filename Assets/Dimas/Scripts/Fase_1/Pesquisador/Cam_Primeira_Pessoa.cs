bf31b5e8850161f801686e01e7440583968e4ec1
Gustavo Dimas <gustavodimas.92@gmail.com>
2024-04-09 17:48:58 -0300

    Adicionado: Script de movimentação e Câmera em primeira pessoa + Analógico + Package de Teste + TextMeshPro e etc.

---
+++ Assets/Dimas/Scripts/Fase_1/Pesquisador/Cam_Primeira_Pessoa.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Cam_Primeira_Pessoa : MonoBehaviour
+{
+    [SerializeField] Transform pesquisador;
+    public Player_Pesquisador playerPesquisador;
+    public float sensibilidade = 0.01f;
+    public float suavidade = 1.5f;
+    public float fatorEscala = 100f;
+
+    Vector2 velocidade;
+    Vector2 velocidadeFrame;
+
+    void Reset() => pesquisador = GetComponentInParent<Player_Pesquisador>().transform;
+
+    void Update()
+    {
+        Vector2 toqueDelta = Vector2.zero;
+
+        if (Input.touchCount > 0)
+        {
+            Touch toque = Input.GetTouch(0);
+
+            if (!RectTransformUtility.RectangleContainsScreenPoint(playerPesquisador.areaAnalogico, toque.position))
+                toqueDelta = toque.deltaPosition / fatorEscala;
+        }
+
+        Vector2 rawFrameVelocity = Vector2.Scale(toqueDelta, Vector2.one * sensibilidade);
+        velocidadeFrame = Vector2.Lerp(velocidadeFrame, rawFrameVelocity, 1 / suavidade);
+        velocidade += velocidadeFrame;
+        velocidade.y = Mathf.Clamp(velocidade.y, -90, 90);
+
+        transform.localRotation = Quaternion.AngleAxis(-velocidade.y, Vector3.right);
+        pesquisador.localRotation = Quaternion.AngleAxis(velocidade.x, Vector3.up);
+    }
+}
---
+++ Assets/Dimas/Scripts/Fase_1/Pesquisador/Player_Pesquisador.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class Player_Pesquisador : MonoBehaviour
+{
+    [Header("Configuracoes:")]
+    [SerializeField] bool desativarEntradas = false;
+    [SerializeField] float velocidadeMovimento;
+
+    [Header("Atribuicoes:")]
+    [SerializeField] RectTransform analogico;
+    public RectTransform areaAnalogico;
+    [SerializeField] RectTransform pontoAnalogico;
+    [SerializeField] Transform cameraTransform;
+
+    Vector3 direcaoMovimento;
+    Vector2 posicaoPonto, posicaoInicialPonto;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        posicaoInicialPonto = pontoAnalogico.position;
+    }
+
+    void Update()
+    {
+        if (!desativarEntradas) Joystick();
+    }
+
+    void FixedUpdate()
+    {
+        if (direcaoMovimento != Vector3.zero)
+        {
+            Vector3 _movimentoHorizontal = cameraTransform.TransformDirection(new Vector3(direcaoMovimento.x, 0, direcaoMovimento.y));
+            rb.MovePosition(transform.position +  velocidadeMovimento * Time.deltaTime * _movimentoHorizontal);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_movimentoHorizontal), 0.15f);
+        }
+    }
+
+    void Joystick()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch _toque = Input.GetTouch(0);
+            Vector2 _posicaoToque = _toque.position;
+
+            if (RectTransformUtility.RectangleContainsScreenPoint(areaAnalogico, _posicaoToque))
+            {
+                if (_toque.phase == TouchPhase.Moved)
+                {
+                    posicaoPonto = _toque.position - (Vector2)analogico.position;
+                    posicaoPonto = Vector2.ClampMagnitude(posicaoPonto, 50f);
+
+                    pontoAnalogico.position = (Vector2)analogico.position + posicaoPonto;
+
+                    direcaoMovimento = (pontoAnalogico.position - analogico.position).normalized;
+                }
+
+                if (_toque.phase == TouchPhase.Ended || _toque.phase == TouchPhase.Canceled)
+                {
+                    ResetarPosicaoJoystick();
+                    direcaoMovimento = Vector3.zero;
+                }
+            }
+            else
+            {
+                ResetarPosicaoJoystick();
+                direcaoMovimento = Vector3.zero;
+            }
+        }
+    }
+
+    void ResetarPosicaoJoystick()
+    {
+        posicaoPonto = Vector2.zero;
+        pontoAnalogico.position = posicaoInicialPonto;
+    }
+}
