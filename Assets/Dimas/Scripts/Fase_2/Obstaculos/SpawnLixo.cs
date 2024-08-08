6fecc6c3d4e5b5db18ee3c007a03822f59033680
Gustavo Dimas <gustavodimas.92@gmail.com>
2024-08-08 01:52:26 -0300

    Build Test

--- Assets/Dimas/Scripts/Fase_2/Obstaculos/SpawnLixo.cs
+++ Assets/Dimas/Scripts/Fase_2/Obstaculos/SpawnLixo.cs
@@ -6,6 +6,7 @@ public class SpawnLixo : MonoBehaviour
     [SerializeField] GameObject prefabLixo;
     [SerializeField] Transform pontoSpawn;
     [SerializeField] float intervaloGeracao = 10f;
+    [SerializeField] float limitePosicaoZ;
 
     private void Start() => StartCoroutine(GerarLixoPeriodicamente());
 
@@ -15,8 +16,15 @@ public class SpawnLixo : MonoBehaviour
         {
             yield return new WaitForSeconds(intervaloGeracao);
 
+            if (pontoSpawn.position.z >= limitePosicaoZ)
+            {
+                yield break;
+            }
+
             if (FindObjectsOfType<Lixo>().Length == 0)
+            {
                 Instantiate(prefabLixo, pontoSpawn.position, Quaternion.identity);
+            }
         }
     }
 }
